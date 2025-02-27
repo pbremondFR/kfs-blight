33753cad0125221086455f0c05474000eeb65a65
Paul Brémond <pbremond@student.42nice.fr>
2025-02-27 22:12:12 +0100

    Bjarne Stroustrup says constexpr > #define

--- src/screen.rs
+++ src/screen.rs
@@ -1,18 +1,10 @@
 use core::fmt::{self, Write};
 use core::ptr::copy;
 
-macro_rules! VGA_BUFFER {
-    () => {0xb8000}
-}
-macro_rules! VGA_WIDTH {
-    () => {80}
-}
-macro_rules! VGA_HIGHT {
-    () => {25}
-}
-macro_rules! VGA_BUFFER_SIZE {
-    () => {VGA_WIDTH!() * 2 * VGA_HIGHT!()}
-}
+const VGA_BUFFER: usize = 0xb8000;
+const VGA_WIDTH: usize = 80;
+const VGA_HEIGHT: usize = 25;
+const VGA_BUFFER_SIZE: usize = VGA_WIDTH * 2 * VGA_HEIGHT;
 
 macro_rules! pr_debug {
     ($dst:expr, $($arg:tt)*) => {
@@ -47,7 +39,7 @@ macro_rules! pr_error {
 
 
 pub struct Screen {
-    buf: [u8; VGA_BUFFER_SIZE!()],
+    buf: [u8; VGA_BUFFER_SIZE],
     line: usize,
     pos: usize,
     color: u8,
@@ -55,7 +47,7 @@ pub struct Screen {
 
 impl Screen {
     pub fn new() -> Self {
-        Screen { buf: [0; VGA_BUFFER_SIZE!()], line: 0, pos: 0, color: 0x0 }
+        Screen { buf: [0; VGA_BUFFER_SIZE], line: 0, pos: 0, color: 0x0 }
     }
 
     pub fn set_color(&mut self, color: u8) {
@@ -63,21 +55,21 @@ impl Screen {
     }
 
     pub fn scroll_up(&mut self) {
-        if self.line < VGA_HIGHT!() {
+        if self.line < VGA_HEIGHT {
             self.line += 1;
 
         } else {
             self.line -= 1;
 
-            for i in 0..(VGA_HIGHT!() - 1) {
-                for j in 0..VGA_WIDTH!() {
-                    self.buf[(j + i * VGA_WIDTH!()) * 2] = self.buf[(j + (i + 1) * VGA_WIDTH!()) * 2];
-                    self.buf[(j + i * VGA_WIDTH!()) * 2 + 1] = self.buf[(j + (i + 1) * VGA_WIDTH!()) * 2 + 1];
+            for i in 0..(VGA_HEIGHT - 1) {
+                for j in 0..VGA_WIDTH {
+                    self.buf[(j + i * VGA_WIDTH) * 2] = self.buf[(j + (i + 1) * VGA_WIDTH) * 2];
+                    self.buf[(j + i * VGA_WIDTH) * 2 + 1] = self.buf[(j + (i + 1) * VGA_WIDTH) * 2 + 1];
                 }
             }
-            for i in 0..VGA_WIDTH!() {
-                self.buf[(i + 24 * VGA_WIDTH!()) * 2 ] = 0;
-                self.buf[(i + 24 * VGA_WIDTH!()) * 2 + 1] = 0;
+            for i in 0..VGA_WIDTH {
+                self.buf[(i + 24 * VGA_WIDTH) * 2 ] = 0;
+                self.buf[(i + 24 * VGA_WIDTH) * 2 + 1] = 0;
             }
         }
     }
@@ -85,31 +77,31 @@ impl Screen {
 
 impl Write for Screen {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        if s.len() > VGA_WIDTH!() {
+        if s.len() > VGA_WIDTH {
             return Err(fmt::Error);
         }
-        
-        if self.line == VGA_HIGHT!(){
+
+        if self.line == VGA_HEIGHT {
             self.scroll_up();
         }
 
         for byte in s.bytes() {
-            if byte == b'\n' || self.pos == VGA_WIDTH!() * 2 {
+            if byte == b'\n' || self.pos == VGA_WIDTH * 2 {
                 self.pos = 0;
                 self.scroll_up();
                 continue;
-            } 
-            self.buf[(self.pos + self.line * VGA_WIDTH!()) * 2 ] = byte;
-            self.buf[(self.pos + self.line * VGA_WIDTH!()) * 2 + 1] = self.color;
+            }
+            self.buf[(self.pos + self.line * VGA_WIDTH) * 2 ] = byte;
+            self.buf[(self.pos + self.line * VGA_WIDTH) * 2 + 1] = self.color;
             self.pos += 1;
         }
 
-        let vga_buffer = VGA_BUFFER!() as *mut u8;
-        
+        let vga_buffer = VGA_BUFFER as *mut u8;
+
         unsafe {
-            copy(self.buf.as_ptr(), vga_buffer, VGA_BUFFER_SIZE!());
+            copy(self.buf.as_ptr(), vga_buffer, VGA_BUFFER_SIZE);
         }
-        
+
         Ok(())
     }
 
